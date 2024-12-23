215e50e8edcaab8c9b443053084b055726055ff8
Valeriy <masterbox06@gmail.com>
2024-12-23 22:14:22 +0400

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  plugins: [],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+}
